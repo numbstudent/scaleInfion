9ad56318c1f4098eecc4f27c64249a74eda4b7df
damar <damar.pradiptojati@sby.spil>
2022-09-23 12:22:30 +0700

    mboh capek wkwk

--- mainApp/conveyor.py
+++ mainApp/conveyor.py
@@ -1,18 +1,22 @@
 import serial
 import time
 def run_conveyor():
-    try:
-        ser = serial.Serial('/dev/ttyUSB3', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
-        msg = bytearray()
+    result = False
+    for i in range(0,4):
+        if not result:
+            try:
+                ser = serial.Serial('/dev/ttyUSB'+i, baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+                msg = bytearray()
 
-        msg.append(0x41)
-        msg.append(0xD)
-        ser.write(msg)
-        time.sleep(2)
-        msg.append(0x42)
-        msg.append(0xD)
-        ser.write(msg)
-        ser.close()
-        return True
-    except:
-        return False
+                msg.append(0x41)
+                msg.append(0xD)
+                ser.write(msg)
+                time.sleep(2)
+                msg.append(0x42)
+                msg.append(0xD)
+                ser.write(msg)
+                ser.close()
+                result = True
+            except:
+                result = False
+    return result

de7f94f7f4f9381f9ff918668540c9deab4a0d87
damar <damar.pradiptojati@sby.spil>
2022-09-23 10:37:58 +0700

    update conveyor lagi

--- mainApp/conveyor.py
+++ mainApp/conveyor.py
@@ -5,12 +5,15 @@ def run_conveyor():
         ser = serial.Serial('/dev/ttyUSB3', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
         msg = bytearray()
 
-        # Change this
         msg.append(0x41)
-
+        msg.append(0xD)
+        ser.write(msg)
+        time.sleep(2)
+        msg.append(0x42)
         msg.append(0xD)
         ser.write(msg)
         ser.close()
+
         return True
     except:
         return False

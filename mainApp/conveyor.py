6307e8b8d5f61617b97d218030f312a474cd8b31
damar <damar.pradiptojati@sby.spil>
2022-09-23 10:40:00 +0700

    mumet

--- mainApp/conveyor.py
+++ mainApp/conveyor.py
@@ -1,19 +1,20 @@
 import serial
 
 def run_conveyor():
-    try:
-        ser = serial.Serial('/dev/ttyUSB3', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
-        msg = bytearray()
+    # try:
+    ser = serial.Serial('/dev/ttyUSB3', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+    msg = bytearray()
+    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
 
-        msg.append(0x41)
-        msg.append(0xD)
-        ser.write(msg)
-        time.sleep(2)
-        msg.append(0x42)
-        msg.append(0xD)
-        ser.write(msg)
-        ser.close()
+    msg.append(0x41)
+    msg.append(0xD)
+    ser.write(msg)
+    time.sleep(2)
+    msg.append(0x42)
+    msg.append(0xD)
+    ser.write(msg)
+    ser.close()
 
-        return True
-    except:
-        return False
+    # return True
+    # except:
+    #     return False

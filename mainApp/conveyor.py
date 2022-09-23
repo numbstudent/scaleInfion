074de313f935d042f93fa4c2a79c59ee6fe1db4d
damar <damar.pradiptojati@sby.spil>
2022-09-23 10:42:06 +0700

    mumet2

--- mainApp/conveyor.py
+++ mainApp/conveyor.py
@@ -4,7 +4,6 @@ def run_conveyor():
     # try:
     ser = serial.Serial('/dev/ttyUSB3', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
     msg = bytearray()
-    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
 
     msg.append(0x41)
     msg.append(0xD)

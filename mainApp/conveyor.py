8572624ac7e91dd933b1b0e1c3d4f911b36b2a7e
damar <damar.pradiptojati@sby.spil>
2022-09-26 13:01:38 +0700

    conveyor change usb name

--- mainApp/conveyor.py
+++ mainApp/conveyor.py
@@ -1,28 +1,21 @@
 import serial
 import time
 
-nousb = 0
 def run_conveyor():
-    result = False
-    for i in range(0,4):
-        if not result:
-            try:
-                ser = serial.Serial('/dev/ttyUSB'+str(i), baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
-                msg = bytearray()
+    try:
+        ser = serial.Serial('/dev/relay', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+        msg = bytearray()
 
-                msg.append(0x41)
-                msg.append(0xD)
-                ser.write(msg)
-                time.sleep(2)
-                msg.append(0x42)
-                msg.append(0xD)
-                ser.write(msg)
-                ser.close()
-                result = True
-                nousb = i
-            except:
-                result = False
+        msg.append(0x41)
+        msg.append(0xD)
+        ser.write(msg)
+        time.sleep(2)
+        msg.append(0x42)
+        msg.append(0xD)
+        ser.write(msg)
+        ser.close()
+        result = True
+        nousb = i
+    except:
+        result = False
     return result
-
-# print(run_conveyor())
-# print(nousb)

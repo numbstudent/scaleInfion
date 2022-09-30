dddc4f786025058b6dd7376a11a54e163ffbdead
damar <damar.pradiptojati@sby.spil>
2022-09-30 09:35:24 +0700

    debug conveyor

--- mainApp/conveyor.py
+++ mainApp/conveyor.py
@@ -19,3 +19,18 @@ def run_conveyor():
     except:
         result = False
     return result
+
+def debug_conveyor():
+    ser = serial.Serial('/dev/relay', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+    msg = bytearray()
+
+    msg.append(0x41)
+    msg.append(0xD)
+    ser.write(msg)
+    time.sleep(2)
+    msg.append(0x42)
+    msg.append(0xD)
+    ser.write(msg)
+    ser.close()
+    result = True
+    nousb = i
